fddef86c2be35f802d75367a1eb1557dcdfd5853
PupsDev <Control3d@gmail.com>
2021-04-14 20:08:06 +0200

    backedn

--- backend.py
+++ backend.py
@@ -2,23 +2,29 @@ from wrapper import Api
 import json
 import os
 import sys
+from database import Database
 
 ##
 # use :
-# backend.py BEARER_TOKEN max_pages keyword
+# backend.py BEARER_TOKEN DB_URL max_pages keyword
 ##
 
-if sys.argc > 3:
 
-	api = Api(sys.argv[1], True)
+if sys.argc > 4:
+	db = Database(sys.argv[2])
+
+	db.select_database("disastweet")
+
+ 	db.select_collection("spacetweets") #select or create a collection
+
+	api = Api(sys.argv[2], True)
 	tweet_fields = "expansions=geo.place_id&tweet.fields=geo,entities,author_id"
 	max_results = 50
-	max_pages = sys.argv[2]
+	max_pages = sys.argv[3]
 
-	search = sys.argv[3]
+	search = sys.argv[4]
 
 	query = search+"&max_results="+str(max_results)
 	response = api.get_yielded_recent_search(query,tweet_fields,max_pages)
 	for tweets in response:
-		with open("search.json", "w", encoding='utf-8') as outfile:
-				json.dump(tweets, outfile)
+		 db._insert("", tweets)
